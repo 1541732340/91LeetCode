b50af16f1ccecc60c17813c92c22c950c8924600
1541732340 <1541732340@qq.com>
2022-10-28 16:55:07 +0800

    TASK-1001
    数组练习02
    
    Co-Authored-By: Peter H. Fröhlich <peter.hans.froehlich@gmail.com>

--- src/src/main/java/org/element01/array/Test02.java
+++ src/src/main/java/org/element01/array/Test02.java
@@ -60,28 +60,32 @@ public class Test02 {
      *      * 3、两者的差值为缺失的数字
      */
     public int missingNumber3(int[] nums) {
-        int left = 0;
-        int right = nums.length -1;
-        int mid = 0;
-        while (left <= right){
-            mid = left + (right - left) >>1;
-            if (mid + 1 < nums.length && nums[mid] == mid && nums[mid + 1] == mid +1){
-                return mid+1;
-            }else if (nums[mid] == mid){
-                left = mid +1;
-            }else {
-                right = mid -1;
-            }
-        }
-        if (nums[0]==0){
-            return nums.length;
-        }else {
+        if (nums[0] > 0) {
+            // 比如 [1, 2, 3] 这种情况无法初始化 left 指针，因为压根不存在 nums[i] == i 的位置
             return 0;
         }
+        if (nums[nums.length - 1] < nums.length) {
+            // 比如 [0, 1, 2] 这种情况无法初始化 right 指针，因为压根不存在 nums[i] != i 的位置
+            return nums.length;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (right - left > 1) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == mid) {
+                // 说明我们找到的 mid 这个位置的元素属于“左边的部分”，可以安全地把 left 指针移过来
+                left = mid;
+            } else if (nums[mid] != mid) {
+                // 说明我们找到的这个 mid 这个位置的元素属于“右边的部分”，可以安全地把 right 指针移过来
+                right = mid;
+            }
+        }
+        // 比如 [1, 2, 4, 5] 这个数组，此时 left 会指向数字 2，right 会指向数字 4，显然 left + 1 就是我们要的答案
+        return left + 1;
     }
     /**
      * 复杂度分析：
-     *      时间复杂度:O(n),
+     *      时间复杂度:O(log n),
      *      空间复杂度:O(1)
      */
 
